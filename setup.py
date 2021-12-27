cda1802a301ca964236af8ad7ddab084fd0c78ff
joncrall <jon.crall@kitware.com>
2021-12-27 17:23:16 -0500

    Add baseline setup.py for pip mangement

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='ldc',
+        version='0.0.1',
+        author='Kitware',
+        scripts=['ldc.sh'],
+        packages=[],
+    )
